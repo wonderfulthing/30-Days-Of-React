600dbd40dd76df96a8b4c908e077335dde18482b
l-wangchangzu <l-wangchangzu@qianxin.com>
2022-10-04 15:59:29 +0800

    feat: day 1 finish

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,21 @@
+// express the following expressions
+// 1. a + b + c
+// 2. a * b * c
+// 3. a * b + c
+// 4. a + b * c
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+console.log(a + b + c);
+console.log(a * b * c);
+console.log(a * b + c);
+console.log(a + b * c);
+
+
+// Level 2
+// Language: javascript
+// Path: solutions/day-01/level2.js
+// express the following expressions
+// 1. a + b + c + d
